2abff96cf6bc82121de28989bcb5c2b314d85688
Ryan Challacombe <ryanchallacombe@gmail.com>
2025-11-04 21:00:42 -0600

    manual-linux script updates 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -49,15 +49,15 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     #echo "RUNNING MAKE CONFIG"
     #make menuconfig
 
-    if [ -f "${CONFIG_FILE_LOC}/.config" ]; then
+    #if [ -f "${CONFIG_FILE_LOC}/.config" ]; then
         # copy the .config file to appropriate location
-        echo "**** COPYING A STATIC .config FILE TO ${OUTDIR}/linux-stable"
-        cp ${CONFIG_FILE_LOC}/.config ${OUTDIR}/linux-stable
-    else
-        echo "**** UNABLE TO FIND .config FILE AT ${CONFIG_FILE_LOC}"
-        echo "RUNNING MAKE CONFIG"
-        make menuconfig
-    fi
+    #    echo "**** COPYING A STATIC .config FILE TO ${OUTDIR}/linux-stable"
+    #    cp ${CONFIG_FILE_LOC}/.config ${OUTDIR}/linux-stable
+    #else
+    #    echo "**** UNABLE TO FIND .config FILE AT ${CONFIG_FILE_LOC}"
+    #    echo "RUNNING MAKE CONFIG"
+    #    make menuconfig
+    #fi
 
 
     echo "RUNNING CLEAN STEP"
