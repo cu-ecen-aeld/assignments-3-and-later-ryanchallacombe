7d3bc35eede910d18af753458d1fd80ecf6b7ad4
Ryan Challacombe <ryanchallacombe@gmail.com>
2025-11-05 19:08:36 -0600

    added a check for access to /tmp and cross compiler sysroot locations

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -20,11 +20,37 @@ CROSS_COMPILE=aarch64-none-linux-gnu-
 #SYSROOT_CROSS_COMPILER=/home/ryan/projects/aarch64_toolchain_install_dir/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin/../aarch64-none-linux-gnu/libc
 SYSROOT_CROSS_COMPILER=/home/ryan/projects/aarch64_toolchain_install_dir/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
 CONFIG_FILE_LOC=/home/ryan/projects/assignment-1-ryanchallacombe/finder-app/a3p2_kernel_config
+FILE_LOC_1=/tmp
+FNAME_1=deleteme.txt
+FNAME_2=lib/ld-linux-aarch64.so.1
 
 
 # add path to my cross compiler
 export PATH=$PATH:/home/ryan/projects/aarch64_toolchain_install_dir/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin
 export PATH=$PATH:/home/ryan/projects/aarch64_toolchain_install_dir/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
+
+#############################
+if [ -f "${FILE_LOC_1}/${FNAME_1}" ]; then
+    echo "**** File ${FILE_LOC_1}/${FNAME_1} was found"
+    #echo "EXITING WITH 0"
+    #exit 0
+else
+    echo "**** UNABLE TO FIND .${FILE_LOC_1}/${FNAME_1}"
+    #echo "EXITING WITH 1"
+    #exit 1
+fi
+
+if [ -f "${SYSROOT_CROSS_COMPILER}/${FNAME_2}" ]; then
+    echo "**** File ${SYSROOT_CROSS_COMPILER}/${FNAME_2} was found"
+    echo "EXITING WITH 0"
+    exit 0
+else
+    echo "**** UNABLE TO FIND ${SYSROOT_CROSS_COMPILER}/${FNAME_2}"
+    echo "EXITING WITH 1"
+    exit 1
+fi
+
+#############################
 
 if [ $# -lt 1 ]
 then
