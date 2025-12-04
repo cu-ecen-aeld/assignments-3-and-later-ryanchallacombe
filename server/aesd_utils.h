da0b0f0de3b2fa3b34398dd522f233c7f70b1569
Ryan Challacombe <ryanchallacombe@gmail.com>
2025-12-03 21:19:30 -0600

    added aesdsocket start/stop daemon script in server dir

--- server/aesd_utils.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-
-void helloworld(void) {
-	printf("Hello World!!\n");
-}
-
---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+##########################################################################
+# Add a startup script aesdsocket-start-stop in the “server” directory 
+# of your assignment 3 repository which uses start-stop-daemon to start 
+# your aesdsocket application in daemon mode with the -d option.
+#
+# On stop it should send SIGTERM to gracefully exit your daemon and perform 
+# associated cleanup steps.
+##########################################################################
+
+
+case $1 in 
+	start)
+		echo "Starting aesdsocket daemon"
+		start-stop-daemon --start --name aesdsocket --startas /home/ryan/projects/assignment-1-ryanchallacombe/server/aesdsocket -- -d
+		;;
+	stop)
+		echo "Stopping aesdsocket daemon"
+		
+		# SIGTERM is the dfault signal sent on stop
+		start-stop-daemon --stop --name aesdsocket		
+		;;
+	*)
+		echo "Usage: $0 {start | stop}"
+	exit 1
+esac
+
+exit 0
